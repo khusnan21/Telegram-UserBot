8bd5a97f03154569921e98f44438c7734cca0a08
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2019-06-18 18:22:04 +0530

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -135,7 +135,7 @@ async def amireallyalive(alive):
     if not alive.text[0].isalpha() and alive.text[0] not in ("/", "#", "@", "!"):
         await alive.edit(
             "`"
-            "Your bot is running \n\n"
+            "My bot is running \n\n"
             f"Telethon version: {version.__version__} \n"
             f"Python: {python_version()} \n"
             f"User: {DEFAULTUSER}"
