2a9f89529999e9c4704aaaaa5252c5364151d56e
Krptic <50475008+shxnpie@users.noreply.github.com>
2019-07-01 18:58:41 +0530

    Fixing Indention Error

--- string_session.py
+++ string_session.py
@@ -8,9 +8,9 @@ Click on API Development Tools
 Create a new application, by entering the required details""")
 
 with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
-print("Here is your userbot srting, copy it to a safe place !!")
-print("")
-print(client.session.save())
-print("")
-print("")
-print("Enjoy your userbot !!")
+    print("Here is your userbot srting, copy it to a safe place !!")
+    print("")
+    print(client.session.save())
+    print("")
+    print("")
+    print("Enjoy your userbot !!")
